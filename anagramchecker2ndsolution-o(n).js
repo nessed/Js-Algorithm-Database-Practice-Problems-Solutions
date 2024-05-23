8fbb11356ca7b5804cfa1778ad356fdf9b52938d
nessed <ali.abid44444@gmail.com>
2024-05-23 12:57:30 +0500

    m

--- anagramchecker2ndsolution-o(n).js
+++ anagramchecker2ndsolution-o(n).js
@@ -1,18 +1,26 @@
 function validAnagram2(string1, string2) {
-    if (string1.length !== string2.length) return false;
-    const sorted1 = string1.split("").sort();
-    const sorted2 = string2.split("").sort();
-  
-    for (let i = 0; i < sorted1.length; i++) {
-      if (sorted1[i] !== sorted2[i]) {
-        return false;
-      }
-    }
-  
-    return true;
+  if (typeof string2 !== 'string' || typeof string2 !== 'string') return false
+  if (string1 ===  '' && string2 === '') return true
+
+  if (!string1 || !string2) {
+    console.log('false')
+    return false
   }
 
-  
-  module.exports = {
-    validAnagram2
+  if (string1.length !== string2.length) return false;
+
+  const sorted1 = string1.split("").sort();
+  const sorted2 = string2.split("").sort();
+
+  for (let i = 0; i < sorted1.length; i++) {
+    if (sorted1[i] !== sorted2[i]) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+module.exports = {
+  validAnagram2
 }
