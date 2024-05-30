d7d0f5d03a66e229da799e0f7d0692cfa4bb2317
nessed <ali.abid44444@gmail.com>
2024-05-30 11:56:46 +0500

    array-move o(n) space

--- internship-problems/Array-Move-problem/arrayMove.js
+++ internship-problems/Array-Move-problem/arrayMove.js
@@ -1,7 +1,6 @@
 //take any amount of arrays as input and merge them all into one array
 
 function move(arr) {
-  let arr1 = [];
   for (let i = 0; i < arr.length; i++) {
     if (arr[i] < 0) {
       arr1.push(arr[i]);
