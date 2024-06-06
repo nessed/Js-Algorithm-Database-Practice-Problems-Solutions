4a56966371c1ef54a612c9b782236b89649ac645
nessed <ali.abid44444@gmail.com>
2024-06-06 11:24:24 +0500

    2nd solution to array move

---
+++ data-structures/singly-linked-list/singlyList.js
@@ -0,0 +1,28 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+class SinglyLinkdList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+  push(val){
+
+  }
+}
+var list = new SinglyLinkdList()
+list.push("Hello")
+list.push("Goodbye")
+
+// let first = new Node("Hi");
+// first.next = new Node("there");
+// first.next.next = new Node("ali");
+// first.next.next.next = new Node("are");
+// first.next.next.next.next = new Node("you");
+
+// console.log(first);
