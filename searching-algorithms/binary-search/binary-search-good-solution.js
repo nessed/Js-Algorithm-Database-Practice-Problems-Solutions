ce12af2d7d7b655f2aad766574f4e96e567aa43a
nessed <ali.abid44444@gmail.com>
2024-06-07 12:28:37 +0500

    binary search

---
+++ searching-algorithms/binary-search/binary-search-good-solution.js
@@ -0,0 +1,34 @@
+function delFirstHalf(arr, half) {
+    return (arr = arr.slice(0, half - 1));
+  }
+  
+  function delSecondHalf(arr, half) {
+    return arr.slice(half, arr.length);
+  }
+  
+  function binarySearch(arr, val) {
+    if (!arr || arr.length === 0) {
+      console.log(-1); // Element not found
+      return;
+    }
+  
+    let low = 0;
+    let high = arr.length - 1;
+  
+    while (low <= high) {
+      const mid = Math.floor((low + high) / 2);
+  
+      if (arr[mid] === val) {
+        console.log("Element found at index:", mid);
+        return;
+      } else if (val < arr[mid]) {
+        high = mid - 1; // Search in the left half
+      } else {
+        low = mid + 1; // Search in the right half
+      }
+    }
+  
+    console.log(-1); // Element not found
+  }
+  
+  binarySearch([-1, 0, 1, 2, 3], 2);
