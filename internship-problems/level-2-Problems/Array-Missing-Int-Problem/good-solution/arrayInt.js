8bb62433c9820bdc3ecd8d62938b1c8743b4a4fb
nessed <ali.abid44444@gmail.com>
2024-06-11 10:08:58 +0500

    array missing integer problem

--- internship-problems/level-2-Problems/Array-Missing-Int-Problem/good-solution/arrayInt.js
+++ internship-problems/level-2-Problems/Array-Missing-Int-Problem/good-solution/arrayInt.js
@@ -1,9 +1,13 @@
 function missingInt(arr, n) {
-  let expectedSum = (n * (n + 1)) / 2;
-  let actualSum = 0;
-  for (let i = 0; i < arr.length; i++) {
-    actualSum += arr[i];
+  if (arr.length == 0) {
+    return false;
+  } else {
+    let expectedSum = (n * (n + 1)) / 2;
+    let actualSum = 0;
+    for (let i = 0; i < arr.length; i++) {
+      actualSum += arr[i];
+    }
+    console.log(expectedSum - actualSum);
   }
-  console.log(expectedSum - actualSum);
 }
 missingInt([1, 2, 3, 5], 5);
