dd5bd60b1ffdc1f27e32e816352a767a8302c7fe
nessed <ali.abid44444@gmail.com>
2024-06-11 10:16:29 +0500

    array pair counter

---
+++ internship-problems/level-2-Problems/Array-Count-Pairs-Problem/countPairs.js
@@ -0,0 +1,17 @@
+// Count pairs with given sum
+// Last Updated : 13 Apr, 2024
+// Given an array of N integers, and an integer K, the task is to find the number of pairs of integers in the
+// array whose sum is equal to K.
+
+// Examples:  
+
+// Input: arr[] = {1, 5, 7, -1}, K = 6
+// Output:  2
+// Explanation: Pairs with sum 6 are (1, 5) and (7, -1).
+
+// Input: arr[] = {1, 5, 7, -1, 5}, K = 6
+// Output:  3
+
+function countPairs(arr,num){
+    
+}
