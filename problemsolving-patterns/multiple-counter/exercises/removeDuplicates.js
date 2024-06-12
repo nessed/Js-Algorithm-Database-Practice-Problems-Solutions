2e460e0333fe29174f9490ffcfaf08f4096d4259
nessed <ali.abid44444@gmail.com>
2024-06-12 11:23:53 +0500

    new multiple counter exercise remove duplicates

---
+++ problemsolving-patterns/multiple-counter/exercises/removeDuplicates.js
@@ -0,0 +1,58 @@
+// Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that 
+// each unique element appears only once. The relative order of the elements should be kept the same.
+//  Then return the number of unique elements in nums.
+
+// Consider the number of unique elements of nums to be k, to get accepted, you need to do the following things:
+
+// Change the array nums such that the first k elements of nums contain the unique elements in the 
+// order they were present in nums initially. The remaining elements of nums are not important as well as the size of nums.
+// Return k.
+// Custom Judge:
+
+// The judge will test your solution with the following code:
+
+// int[] nums = [...]; // Input array
+// int[] expectedNums = [...]; // The expected answer with correct length
+
+// int k = removeDuplicates(nums); // Calls your implementation
+
+// assert k == expectedNums.length;
+// for (int i = 0; i < k; i++) {
+//     assert nums[i] == expectedNums[i];
+// }
+// If all assertions pass, then your solution will be accepted.
+
+ 
+
+// Example 1:
+
+// Input: nums = [1,1,2]
+// Output: 2, nums = [1,2,_]
+// Explanation: Your function should return k = 2, with the first two elements of nums being 1 and 2 respectively.
+// It does not matter what you leave beyond the returned k (hence they are underscores).
+// Constraints:
+
+// 1 <= nums.length <= 3 * 104
+// -100 <= nums[i] <= 100
+// nums is sorted in non-decreasing order.
+
+function removeDuplicates(arr){
+    let i = 0 
+    arr.sort()
+    for(let j = 1; j < arr.length;j++){
+        if(arr[j] == arr[i]){
+            arr.slice(j)
+            console.log(arr) 
+        }            
+
+    }            console.log(arr[i])
+
+    //const k = i+1
+   // console.log(k)
+    //console.log(arr.slice(0,k));
+    console.log(arr)
+
+    return 
+}
+
+removeDuplicates([1,2,1,2,1,1,3])
