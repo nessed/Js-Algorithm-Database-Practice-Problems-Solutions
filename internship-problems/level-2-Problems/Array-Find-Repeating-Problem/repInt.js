87578e38c1485f25fbd71f10f25387ca79ded617
nessed <ali.abid44444@gmail.com>
2024-06-12 09:50:05 +0500

    updated some titles in level-2 problems

---
+++ internship-problems/level-2-Problems/Array-Find-Repeating-Problem/repInt.js
@@ -0,0 +1,34 @@
+// Given an array arr[] of size n, find the first repeating element. The element should occur more than once and 
+// the index of its first occurrence should be the smallest.
+
+// Note:- The position you return should be according to 1-based indexing. 
+
+// Example 1:
+
+// Input:
+// n = 7
+// arr[] = {1, 5, 3, 4, 3, 5, 6}
+// Output: 2
+// Explanation: 
+// 5 is appearing twice and 
+// its first appearence is at index 2 
+// which is less than 3 whose first 
+// occuring index is 3.
+
+// Example 2:
+
+// Input:
+// n = 4
+// arr[] = {1, 2, 3, 4}
+// Output: -1
+// Explanation: 
+// All elements appear only once so 
+// answer is -1.
+
+function firstRepeat(arr){
+    console.log(arr)
+    for(let i of arr){
+        
+    }
+}
+firstRepeat([1,2,3,2,3])
--- internship-problems/level-2-Problems/incompleteArray-Count-Pairs-Problem/countPairs.js
+++ internship-problems/level-2-Problems/incompleteArray-Count-Pairs-Problem/countPairs.js
@@ -3,7 +3,7 @@
 // Given an array of N integers, and an integer K, the task is to find the number of pairs of integers in the
 // array whose sum is equal to K.
 
-// Examples:  
+// Examples:
 
 // Input: arr[] = {1, 5, 7, -1}, K = 6
 // Output:  2
@@ -12,6 +12,17 @@
 // Input: arr[] = {1, 5, 7, -1, 5}, K = 6
 // Output:  3
 
-function countPairs(arr,num){
-    
+function countPairs(arr, num) {
+  let left = 0;
+  let right = arr.length - 1;
+  arr.sort()
+  while (left < right) {
+    if (arr[left] + arr[right] != num) {
+      right = right - 1;
+    } else {
+      console.log(arr[left], arr[right]);
+    }
+  }
 }
+
+countPairs([1, 5, 7, -1, 5], 5);
